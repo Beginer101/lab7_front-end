55cc3b4e110642bdfab484a50ff87498cc5e1b12
Beginer101 <106706796+Beginer101@users.noreply.github.com>
2024-12-12 00:47:35 +0200

    Add project1

---
+++ src/components/Image.js
@@ -0,0 +1,10 @@
+import React from "react"
+import przemysl_widok from "./img/Przemysl_widok.jpg"
+
+function Image() {
+    return(
+        <img src={przemysl_widok} alt="Image"/>
+    );
+}
+
+export default Image;
